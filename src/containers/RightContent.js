fb0ba1c808b993a2d48b3d47e76b1028722bb708
FinixYang <jz_yangf@163.com>
2021-01-26 02:14:19 +0800

    Clear files

--- src/containers/RightContent.js
+++ src/containers/RightContent.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import { Content, MessageBox, TabBarBox, AgentList } from "../components";
 import { getAgentList } from "../services/serviceApi";
-//import { useDispatch, useSelector } from "react-redux";
-//import { getAgentData } from "../redux/actions";
-//import { agentDataInfo } from "../redux/selector";
 
 const RightContent = () => {
     const [ agents, setAgents ] = React.useState({});
     const [ datafilter, setDataFilter ] = React.useState('');
-    //const dispatch = useDispatch();
-    const getAgentData = () => {
-        getAgentList().then((data)=> {
-            setAgents(data);
-            //dispatch(data);
-        });
-    }
-    React.useEffect(()=> {
-        getAgentData();
-        //dispatch(getAgentData());
-    },[]);
+   
     React.useEffect(()=> {
         getAgentList().then((data)=> {
             setAgents(data.filter(agent => agent.name.indexOf(datafilter) > -1));
         });
     },[datafilter]);
-    // const testData = useSelector(agentDataInfo());
     const agentProps = {
         agents,
         setAgents,
--- src/redux/constant.js
+++
@@ -1,4 +0,0 @@
-export const GET_AGENT_DATA = 'GET_AGENT_DATA';
-export const GET_AGENT_DATA_DONE = 'GET_AGENT_DATA';
-export const GET_AGENT_DATA_ERROR = 'GET_AGENT_DATA';
-export const REDUCER_KEY = 'cruise_demo';
